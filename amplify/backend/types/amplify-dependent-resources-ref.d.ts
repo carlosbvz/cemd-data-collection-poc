240ed1bbbfb96e5d76303bbad625dfb583a72d0a
Carlos Benavides <carlosbvz@gmail.com>
2022-05-23 13:23:21 -0600

    Rebuild amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "cemddatacollectionpo22c917c9": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
